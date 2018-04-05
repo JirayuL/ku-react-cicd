c09a4b8782647c6a924cdc7189c3792d4d26483d
Jirayu Laungwilawan <jirayu.l@ku.th>
2018-04-05 17:50:33 +0700

    Test

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
+          <h2 className="App-title">My name is Gear!</h2>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
